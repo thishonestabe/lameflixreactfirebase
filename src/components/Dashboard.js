27ad312c33c730d4321b2ebdc7a48e47c4448ee1
thishonestabe <marquez.abraham1991@gmail.com>
2020-12-08 18:42:46 +0100

    logout implemented in dashboard component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import {Card, Button, Alert} from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthContext'
-
+import { useHistory } from "react-router";
 
 export default function Dashboard() {
     const [error, setError] = useState('');
     const { currentUser, logout } = useAuth()
-    function handleLogout() {
-
+    const history = useHistory();
+    async function handleLogout() {
+        setError('')
+        try {
+            await logout()
+            history.push('./login')
+        } catch {
+            setError('Failed to logout')
+        }
     }
     return (
         <>
