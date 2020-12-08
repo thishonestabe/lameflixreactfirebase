24560fe2d4b7e34c08bd7ce40e9f375edf029c37
thishonestabe <marquez.abraham1991@gmail.com>
2020-12-08 18:12:03 +0100

    Login link added to signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,7 @@
 import React, {useRef, useState} from 'react'
 import {Card, Form, Button, Alert} from 'react-bootstrap'
 import { useAuth } from "../contexts/AuthContext";
-
+import { Link } from 'react-router-dom'
 export default function Signup() {
     const emailRef = useRef();
     const passwordRef = useRef();
@@ -51,7 +51,7 @@ export default function Signup() {
                 </Card.Body>
             </Card>
             <div className={"w-100 text-center mt-2"}>
-                Log In
+                <Link to={'/login'}>  Log In </Link>
             </div>
         </>
     )
